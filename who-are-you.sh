cf748a76eed7a3b3deee4a682727178fbd5a9866
aibeqa <aibeqaaa@mail.ru>
2019-10-15 18:33:22 +0600

    Completed 4th and ed

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,5 +1,4 @@
-$curl -0 https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json
-echo "$curl"
-jq ".[].id"
-echo "jq"
+curl  https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq ".[] | select(.id == 70).name"
+
+
 
