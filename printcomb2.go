4e0bec1e54ac8947b7e15ec00495079af44c0032
aibeqa <aibeqaaa@mail.ru>
2019-10-16 21:48:24 +0600

    o

--- printcomb2.go
+++ printcomb2.go
@@ -5,12 +5,12 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	var count int = 0
 
-	for num1 := 0; num1 < 100; num1++ {
-		for num2 := 0; num2 < 100; num2++ {
-			if num1 < num2 {
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			if i < j2 {
 				z01.PrintRune(44)
 				z01.PrintRune(32)
-				z01.PrintRune("%02d %02d", num1, num2)
+				z01.PrintRune("%02d %02d", i, j)
 				count++
 			}
 		}
