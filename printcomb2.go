cb82fd5d42abfc23dde77cf67b172ad456d65381
aibeqa <aibeqaaa@mail.ru>
2019-10-16 23:27:29 +0600

    p

--- printcomb2.go
+++ printcomb2.go
@@ -3,18 +3,30 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i := 0; i < 100; i++ {
-		for j := 0; j < 100; j++ {
-			if i < j {
-				z01.PrintRune(i)
-				z01.PrintRune(j)
-				z01.PrintRune(32)
-				z01.PrintRune(i)
-				z01.PrintRune(j)
-				z01.PrintRune(44)
-				z01.PrintRune(32)
+	for o := '0'; o <= '9'; o++ {
+		for v := '0'; v <= '9'; v++ {
+			for f := '0'; f <= '9'; f++ {
+				for e := '0'; e <= '9'; e++ {
+					if o == '9' && v == '8' && f == '9' && e == '9' {
+						z01.PrintRune(o)
+						z01.PrintRune(v)
+						z01.PrintRune(32)
+						z01.PrintRune(f)
+						z01.PrintRune(e)
+						z01.PrintRune(10)
+						break
+					}
+					if o == f && v < e || o < f {
+						z01.PrintRune(o)
+						z01.PrintRune(v)
+						z01.PrintRune(32)
+						z01.PrintRune(f)
+						z01.PrintRune(e)
+						z01.PrintRune(44)
+						z01.PrintRune(32)
+					}
+				}
 			}
 		}
 	}
-	z01.PrintRune(10)
 }
