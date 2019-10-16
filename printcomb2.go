3564275a31b38ce9be2dcc0ecea13a6779fee75f
aibeqa <aibeqaaa@mail.ru>
2019-10-16 21:36:41 +0600

    o

--- printcomb2.go
+++ printcomb2.go
@@ -1,6 +1,6 @@
 package piscine
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	var count int = 0
@@ -8,13 +8,12 @@ func PrintComb2() {
 	for num1 := 0; num1 < 100; num1++ {
 		for num2 := 0; num2 < 100; num2++ {
 			if num1 < num2 {
-				if count > 0 {
-					fmt.Print(", ")
-				}
-				fmt.Printf("%02d %02d", num1, num2)
+				z01.PrintRune(44)
+				z01.PrintRune(32)
+				fz01.PrintRune("%02d %02d", num1, num2)
 				count++
 			}
 		}
 	}
-	fmt.Println()
+	z01.PrintRune()
 }
