de9412037ece85d9819718d676f911a88b679a8b
aibeqa <aibeqaaa@mail.ru>
2019-10-17 20:32:20 +0600

    fgh

---
+++ strrev.go
@@ -0,0 +1,16 @@
+package piscine
+
+func StrRev(s string) string {
+	var tmp_arr []byte = []byte(s)
+	var l int = 0
+	var r int = len(tmp_arr)-1
+
+	for l < r {
+		tmp := tmp_arr[l]
+		tmp_arr[l] = tmp_arr[r]
+		tmp_arr[r] = tmp
+		l++
+		r--
+	}
+	return string(tmp_arr)
+}
