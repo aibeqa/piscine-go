8f215501407406bdabd7cb92af3f8ccc8e7df42e
aibeqa <aibeqaaa@mail.ru>
2019-10-23 20:16:26 +0600

    qwe

--- alphacount.go
+++ alphacount.go
@@ -1,1 +1,12 @@
-paskage 
+package piscine
+
+func AlphaCount(str string) int {
+	answer := 0
+	strc := []rune(str)
+	for _, char := range strc {
+		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') {
+			answer++
+		}
+	}
+	return answer
+}
